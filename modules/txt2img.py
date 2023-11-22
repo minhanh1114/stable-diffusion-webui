167328bd606d052c60a5bd491ce8daa726c82758
minhanh1411 <tuan28dk10cntt@gmail.com>
2023-11-22 23:19:16 +0900

    first commit

--- modules/txt2img.py
+++ modules/txt2img.py
@@ -7,11 +7,16 @@ from modules.shared import opts, cmd_opts
 import modules.shared as shared
 from modules.ui import plaintext_to_html
 import gradio as gr
+from googletrans import Translator #custom
 
 
 def txt2img(id_task: str, prompt: str, negative_prompt: str, prompt_styles, steps: int, sampler_name: str, n_iter: int, batch_size: int, cfg_scale: float, height: int, width: int, enable_hr: bool, denoising_strength: float, hr_scale: float, hr_upscaler: str, hr_second_pass_steps: int, hr_resize_x: int, hr_resize_y: int, hr_checkpoint_name: str, hr_sampler_name: str, hr_prompt: str, hr_negative_prompt, override_settings_texts, request: gr.Request, *args):
     override_settings = create_override_settings_dict(override_settings_texts)
-
+    translate = Translator()
+    # Translate some text
+    result = translate.translate(prompt)
+    prompt=result.text
+    print(result.text)
     p = processing.StableDiffusionProcessingTxt2Img(
         sd_model=shared.sd_model,
         outpath_samples=opts.outdir_samples or opts.outdir_txt2img_samples,
